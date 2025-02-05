b75f8d5208b85afc968744feccd4c748965d2cb0
JotaLucas007 <jglucasilva@gmail.com>
2025-02-05 15:47:15 -0300

    Adicionando arquivo calc

---
+++ 05-02/calc.js
@@ -0,0 +1,8 @@
+// Duas variáveis
+// Num1 e Num2
+// Pergunte para o usuário
+let Num1 = Number(prompt("Digite um número"));
+let Num2 = Number(prompt("Digite outro número"));
+
+// Imprimir resposta
+console.log(`A soma é ${Num1 + Num2}`);
--- 05-02/script.js
+++ 05-02/script.js
@@ -1,8 +1,10 @@
 // variavel
 let nome = prompt("Qual o seu nome?");
 let idade = prompt("Digite sua Idade");
+let time = prompt("Digite seu time");
 
 // imprimir na tela
 document.write("Seja bem vindo" + nome + ", " + "Sua Idade é: " + idade +"Anos.");
 document.write("<br><br>");
 document.write(`Seja bem vindo: ${nome}, sua idade é: ${idade} anos`);
+document.write("")
