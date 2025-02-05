4c37209abab31b95618c354be78ad4a08b2c61c1
JotaLucas007 <jglucasilva@gmail.com>
2025-02-05 14:54:40 -0300

    Adicionando arquivo

---
+++ 05-02/script.js
@@ -0,0 +1,8 @@
+// variavel
+let nome = prompt("Qual o seu nome?");
+let idade = prompt("Digite sua Idade");
+
+// imprimir na tela
+document.write("Seja bem vindo" + nome + ", " + "Sua Idade é: " + idade +"Anos.");
+document.write("<br><br>");
+document.write(`Seja bem vindo: ${nome}, sua idade é: ${idade} anos`);
