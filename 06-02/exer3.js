497729fd2aadc2296585bb90433db51d58522d7f
JotaLucas007 <jglucasilva@gmail.com>
2025-02-06 15:29:14 -0300

    Adicionando exer3

---
+++ 06-02/exer3.js
@@ -0,0 +1,6 @@
+/*
+   JavaScript
+   JS
+*/
+let linguagem = "JavaScript";
+console.log = ""
---
+++ 06-02/tipos-dados.js
@@ -0,0 +1,17 @@
+// String
+let nome = "João";
+
+// Inteiro
+let idade = 16;
+
+// booleano
+let casado = false;
+
+// Números flutuantes com casas decimais
+let valorProduto = 5.35;
+
+console.log("Meu nome", nome);
+console.log(nome);
+console.log("idade", idade);
+console.log9("casado? " + casado);
+console.log("R$", valorProduto);
