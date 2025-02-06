38b5095526c0453e745276fcb587d09382e62be2
JotaLucas007 <jglucasilva@gmail.com>
2025-02-06 14:41:55 -0300

    Adicionando arquivo

---
+++ 06-02/variavel.js
@@ -0,0 +1,12 @@
+let mensagem = "Olá, mundo";
+const nomeCompleto = "João lucas";
+
+// nomeCompleto = "Caio fernandes";
+
+// idade
+let idade = 37;
+
+idade = 38
+
+console.log(nomeCompleto)
+console.log(`${mensagem} e a idade é: ${idade}`);
