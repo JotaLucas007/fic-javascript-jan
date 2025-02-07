e32a48358f989df059efc4e52b09e0cb48080fde
JotaLucas007 <jglucasilva@gmail.com>
2025-02-07 14:35:45 -0300

    Adicionando text2js

--- 07-02/texto.js
+++ 07-02/texto.js
@@ -3,5 +3,10 @@ let text = "Estão chegando as provas";
 // Tamanho da string
 console.log(text.length)
 
+// Replace
+console.log("Alterando", text.replace("provas", "Avalições"));
+confirm.log(text)
+
+
 console.log(text.toLowerCase())
 console.log(text.toUpperCase())
