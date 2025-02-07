32cb8758a7c69a792b5372f30c46364e2dc9b474
JotaLucas007 <jglucasilva@gmail.com>
2025-02-07 14:14:54 -0300

    Adicionando texto e revisao

---
+++ 07-02/revisao.js
@@ -0,0 +1,9 @@
+let mensagem = "Olá João"; // String 
+let idade = 16; // Int
+let casado = true; // Booleano
+let times = ["Paulista", "Santos", 12, true];
+
+console.log("Variável mensagem", mensagem);
+console.log("Variável idade", idade);
+console.log("Variável casado", casado);
+console.log(times);
