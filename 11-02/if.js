4177f74a2750ad7ba8835517baf6ecc2e83fc20c
JotaLucas007 <jglucasilva@gmail.com>
2025-02-11 13:54:33 -0300

    Adicionando if

---
+++ 11-02/if.js
@@ -0,0 +1,12 @@
+/*
+se a idade >= 18, a pessoa pode ir para festa
+ se não, a pessoa não pode ir para festa
+ */
+
+let idade = 37;
+
+if (idade >= 18) {
+    console.log("A pessoa, pode ir para a festa");
+} else {
+    console.log("A pessoa, não pode ir para festa");
+}
