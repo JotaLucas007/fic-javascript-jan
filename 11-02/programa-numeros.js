54a591d2cd4a5e64e027003c34b304259ea50fa2
JotaLucas007 <jglucasilva@gmail.com>
2025-02-11 14:32:23 -0300

    Adicionando numeros js

---
+++ 11-02/programa-numeros.js
@@ -0,0 +1,11 @@
+/*
+     - Verifica se o número é positivo, negativo ou zero
+      se o número for maior que zero, é positivo
+      se o número for menor que zero, é negativo
+      senão o número não é válido
+*/
+ let numero = prompt("Digite um número");
+
+ // Verifica se o número é positivo, negativo ou zero
+  if( numero > 0)
+    document.write(`o numero é positivo`);
