be636bd86c6c1c5417c29ef76382763c5e614e02
JotaLucas007 <jglucasilva@gmail.com>
2025-02-11 14:48:04 -0300

    Adicionando nota

---
+++ 11-02/nota.js
@@ -0,0 +1,13 @@
+/*
+se a nota do aluno for maior ou igual a 6, o aluno está aprovado
+ senão, o aluna esta reprovado
+*/
+let nota = 6;
+
+if (nota >= 6) {
+   console.log("o aluno esta aprovado");
+} else (nota < 6)
+console.log("o aluno esta reprovado");
+
+
+
--- 11-02/programa-numeros.js
+++ 11-02/programa-numeros.js
@@ -4,8 +4,13 @@
       se o número for menor que zero, é negativo
       senão o número não é válido
 */
- let numero = prompt("Digite um número");
+let numero = prompt("Digite um número");
 
- // Verifica se o número é positivo, negativo ou zero
-  if( numero > 0)
-    document.write(`o numero é positivo`);
+// Verifica se o número é positivo, negativo ou zero
+if (numero > 0) {
+    document.write(`o numero ${numero} é positivo`);
+} else if (numero < 0) {
+    document.write(`o numero ${numero} é negativo`);
+} else {
+    document.write(`o numero ${numero}, não é valido`);
+}
