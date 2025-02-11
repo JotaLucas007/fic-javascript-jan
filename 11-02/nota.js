8e5d107b096bd482fdb12282a5ad4a8f3d94a230
JotaLucas007 <jglucasilva@gmail.com>
2025-02-11 14:52:03 -0300

    Adicionando times

--- 11-02/nota.js
+++ 11-02/nota.js
@@ -2,12 +2,12 @@
 se a nota do aluno for maior ou igual a 6, o aluno está aprovado
  senão, o aluna esta reprovado
 */
-let nota = 6;
+let nota = 7;
 
 if (nota >= 6) {
    console.log("o aluno esta aprovado");
-} else (nota < 6)
+} else {
 console.log("o aluno esta reprovado");
 
-
+}
 
