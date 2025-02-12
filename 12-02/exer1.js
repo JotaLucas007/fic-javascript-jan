ad52f2ef12a54b44634be98c484441a0cc765728
JotaLucas007 <jglucasilva@gmail.com>
2025-02-12 14:56:19 -0300

    Adicionando exer2js

---
+++ 12-02/exer1.js
@@ -0,0 +1,14 @@
+/*
+ - Escreva um loop (for) que exiba a frase abaixo
+ no console;
+ - Subustituia o "X" pela informação correta;
+ - "X" devecomeçar em 1 na 1ª exibição da frase e em
+ 10 na última;
+ - Exiba a 10x no console.
+
+ "Esta é a Xª vez que esta frase é exibida no console."
+*/
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Esta é a ${i}ª vez que esta frase é exibida no console`)
+}
