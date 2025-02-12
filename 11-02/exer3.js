2ecc8f92a61000369d413bcead08383196185b8c
JotaLucas007 <jglucasilva@gmail.com>
2025-02-12 13:53:57 -0300

    Adicionando correcao exer3js

---
+++ 11-02/exer3.js
@@ -0,0 +1,13 @@
+/*
+- Armazene sua idade em uma constante;
+- Escreva uma estrutura condicional em que, se a idade é 7 anos ou menos, ou
+  se a idade é 65 anos ou mais, a frase "Para você, a entrada é gratis!" é
+  armzenada em uma variável;
+- Se a idade não atende ás condições acima, a variavel deve armazenar a
+  mensagem "A entrada é R$ 30,00.";
+- Exiba a mensagem no console;
+- Agora, teste diferentes idades para se certificar que a condição do `if`
+  tambem está funcionando.
+*/
+
+let idade
---
+++ 12-02/exer3.js
@@ -0,0 +1,7 @@
+let age = 37;
+
+if (age <= 7 || age >= 65) {
+    console.log(`Para você, a entrade é gratis!`);
+} else {
+    console.log(`A entrada é R$30,00`);
+}
