88259863b5e35e4842602b65fe4830a6d3665f55
JotaLucas007 <jglucasilva@gmail.com>
2025-02-13 16:21:17 -0300

    Adicionando func2js

---
+++ 13-02/func2.js
@@ -0,0 +1,18 @@
+function mostrarNome(nome) {
+    document.write(nome);
+}
+// Função para criar uma frase
+let frase = prompt("Digite uma frase")
+function mostrarfrase(frase) {
+
+}
+
+function pularLinha(){
+    document.write("<br><br>");
+}
+
+mostrarNome(Cristiano);
+pularLinha();
+mostrarNome(Ronaldo);
+pularLinha();
+
