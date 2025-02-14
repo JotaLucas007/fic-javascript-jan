3c01b435f074f6b856ddbd5fecfc6a40591fb86d
JotaLucas007 <jglucasilva@gmail.com>
2025-02-14 16:15:26 -0300

    Adicionando nota

--- 14-02/nota.js
+++ 14-02/nota.js
@@ -2,7 +2,18 @@
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("resultado");
 
-function resultadoNotaClick() {
-    console.log("Olá João");
-    alert("Olá João");
+function resultadoNotaClick(Event) {
+    // previne um comportamento no navegador
+    Event.preventDefault();
+    console.log(inputNota.value);
+
+    // Condição
+    if (inputNota.value >= 6) {
+        resultado.innerHTML = "Aprovado";
+    } else {
+        resultado.innerHTML = "Reprovado";
+    }
+
+    inputNota.value = "";
+
 }
