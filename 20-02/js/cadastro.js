e17189e77b5cbe0618b7f8a8a0827f727a372dc4
JotaLucas007 <jglucasilva@gmail.com>
2025-02-21 14:44:14 -0300

    Adicionando carros

--- 20-02/js/cadastro.js
+++ 20-02/js/cadastro.js
@@ -3,9 +3,15 @@ const campo = document.querySelector("#campo");
 const salvarBtn = document.querySelector("#salvar");
 
 // Adicionar evento de clique ao botão salvar
-salvarBtn.addEventListener("click", function() {
+salvarBtn.addEventListener("click", function () {
     const valor = campo.value;
-    
+
+    // Validação
+    if (campo.value === "") {
+        alert("Digite o campo");
+        return false
+    }
+
     // Salvar no localStorage
     const chave = `ValorSalvo_${Date.now()}`; // usa timestamp para chave única
     //console.log(chave)
