98413e5c1b1fed9fb8a0f094309bc5621345d40b
JotaLucas007 <jglucasilva@gmail.com>
2025-02-26 14:41:47 -0300

    Adicionando props

--- Projeto React/primeiro-projeto/src/App.jsx
+++ Projeto React/primeiro-projeto/src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./Components/Header"
 import "./App.css";
+import Banner from "./Components/Banner";
 
 
 function App() {
@@ -7,14 +8,14 @@ function App() {
   return (
     <>
       <div className="container">
-        <Header />
-       
+        <Header title="Logo New" subTitle="Outros" />
+
 
         <h1>Seja bem vindo!</h1>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae iste tempore excepturi, quos nesciunt obcaecati vitae laudantium. Voluptates tempora ullam sapiente provident maiores inventore quas natus quasi molestiae expedita.
-        </p>
+        <Banner />
+
+
       </div>
     </>
   )
